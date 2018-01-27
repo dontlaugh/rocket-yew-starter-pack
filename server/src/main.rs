8524dcd3ef5a034c06b3d988566606013358c093
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-01-26 21:40:34 -0800

    Implement fetching a task by id
    Our goal here is to make the server-side ID match the client side's
    local storage index. Start with 0, new items that get "pushed" onto the
    list will get the next number, and so on.
    
    This isn't what I'd choose for a serious app, but it will be simple
    enough for this example app.

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,20 +16,18 @@ extern crate tempdir;
 
 #[macro_use]
 extern crate serde_derive;
+extern crate serde_json;
 extern crate serde;
-
 
 use std::sync::{Arc, Mutex};
 use std::path::{Path, PathBuf};
 use serde::{Serialize};
+
 use bincode::{deserialize, serialize, Infinite};
-
-
 use maud::{html, Markup};
 use rocket::State;
 use rocket::response::status;
 use rocket::response::NamedFile;
-
 use rocket_contrib::Json;
 
 
@@ -42,10 +40,8 @@ fn main() {
 }
 
 fn all_routes() -> Vec<rocket::Route> {
-    routes![index, static_file, ugly_hack, create_task]
+    routes![index, static_file, ugly_hack, create_task, get_task]
 }
-
-
 
 #[get("/")]
 fn index(db: State<Arc<sled::Tree>>) -> Markup {
@@ -63,8 +59,8 @@ fn static_file(path: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(path)).ok()
 }
 
-// TODO: remove this when we figure out how to change the native Rust WebAssembly's generated
-// JavaScript code to point at "static/" prefix.
+// TODO: remove this when we figure out how to change the native Rust 
+// WebAssembly's generated JavaScript code to point at "static/" prefix.
 #[get("/ui.wasm")]
 fn ugly_hack() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/ui.wasm")).ok()
@@ -76,23 +72,37 @@ struct Task {
     description: String,
 }
 
-
 #[post("/task", format = "application/json", data = "<task>")]
-fn create_task(
-    db: State<Arc<sled::Tree>>,
-    task: Json<Task>) -> status::Accepted<String> {
+fn create_task(db: State<Arc<sled::Tree>>, task: Json<Task>) -> status::Accepted<String> {
     println!("got a task {:?}", task);
 
+    // scan through our DB to get create an incremented ID.
     let mut count = 0;
     for (_, _) in db.iter() {
         count += 1;
     }
+
+    // Keys and Values in sled are Vec<u8>
     let new_key = vec![count];
-    // our task is the first field "0" on a Json<Task> rocket passes us.
-    // Our task implements Serialize, required here.
+
+    // Our task is the first field (e.g., "0") on Json<Task> Rocket passes us.
     let encoded: Vec<u8> = serialize(&task.0, Infinite).unwrap();
     db.set(new_key, encoded);
     status::Accepted(Some(format!("success")))
+}
+
+#[get("/task/<id>")]
+fn get_task(db: State<Arc<sled::Tree>>, id: u8) -> Option<Json<Task>> {
+
+    let val = db.get(&vec![id]);
+    match val {
+        Some(db_vec) => {
+            let decoded: Task = deserialize(&db_vec[..])
+                .expect("unable to decode Task");
+            Some(Json(decoded))
+        }
+        _ => None
+    }
 }
 
 
@@ -113,10 +123,35 @@ fn test_post_get() {
     let dir = TempDir::new("rocket").unwrap();
     let path = dir.path().join("test_data.db");
     
+    // create our test client
     let c = Client::new(test_instance(path)).unwrap();
+
+    // create a new task with raw json string body
     let req = c.post("/task")
         .body(r#"{"completed": false, "description": "foo"}"#)
         .header(ContentType::JSON);
     let resp = req.dispatch();
     assert_eq!(resp.status(), Status::Accepted);
+
+    let req = c.get("/task/0");
+    let bod = req.dispatch().body_bytes().unwrap();
+    let decoded: Task = serde_json::from_slice(&bod[..]).expect("not a valid task");
+    assert_eq!(&decoded.description, "foo");
+
+
+    // create another task with serde_json's ability to serialize our Task
+    let task = Task{ description: String::from("baz"), completed: true };
+    let req = c.post("/task")
+        .body(serde_json::to_vec(&task).unwrap())
+        .header(ContentType::JSON);
+    let resp = req.dispatch();
+    assert_eq!(resp.status(), Status::Accepted);
+
+    // we expect our next task to have id 1 
+    let req = c.get("/task/1");
+    let bod = req.dispatch().body_bytes().unwrap();
+    let decoded: Task = serde_json::from_slice(&bod[..]).expect("not a valid task");
+    assert_eq!(&decoded.description, "baz");
+    assert_eq!(decoded.completed, true);
+    
 }
